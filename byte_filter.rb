8df689c212ce636ccd27f4eae31d19bfb715befc
Ausin Formica <austin.formica@gmail.com>
2015-12-15 19:48:29 -0500

    simple transpose glitch

---
+++ byte_filter.rb
@@ -0,0 +1,16 @@
+require 'pnglitch'
+
+PNGlitch.open('source_png/Chapterhouse.png') do |p|
+  p.change_all_filters 2
+  p.each_scanline do |l|
+    l.register_filter_encoder do |data, prev|
+      data.size.times.reverse_each do |i|
+        x = data.getbyte(i)
+        v = prev ? prev.getbyte(i) : 0
+        data.setbyte(i, (x - v) & 0xfe)
+      end
+      data
+    end
+  end
+  p.save 'glitch_png/byte_filter.png'
+end
