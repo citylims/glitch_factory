b44bfbcbfbc00b83ab9f82471d15fac7bcb446b8
Ausin Formica <austin.formica@gmail.com>
2015-12-15 20:15:10 -0500

    scanlines arr

--- compression_filter.rb
+++ compression_filter.rb
@@ -1,13 +1,19 @@
 require 'pnglitch'
 
 PNGlitch.open('source_png/NWA.png') do |p|
-  # p.glitch do |data|
-  #   data.gsub /\d/, '2'
-  # end
+  scanlines = Array.new
+  p.glitch do |data|
+    data.gsub /\e/, 'x'
+  end
   p.glitch_after_compress do |data|
-    # puts data
-    data[rand(data.size) / 2] = 'cX'
+    data[rand(data.size)] = 'z'
     data
   end
+  p.each_scanline do |s|
+    s.gsub! /\d/, 'x'
+    s.change_filter 2
+    scanlines << s
+  end
+  puts scanlines.at(0)
   p.save 'glitch_png/compression_filter.png'
 end
