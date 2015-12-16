bc03e43f043d37040351ab3a5696d91c1f85cde7
Ausin Formica <austin.formica@gmail.com>
2015-12-15 19:46:29 -0500

    transpose with average filter

---
+++ transpose_average_filter.rb
@@ -0,0 +1,19 @@
+require 'pnglitch'
+
+PNGlitch.open('source_png/NWA.png') do |p|
+
+  p.change_all_filters 3
+  options = [p.filter.to_s]
+  puts options
+  p.each_scanline do |l|
+    l.register_filter_encoder do |data, prev|
+      data.size.times.reverse_each do |i|
+        x = data.getbyte(i)
+        v = prev ? prev.getbyte(i - 6) : 0
+        data.setbyte(i, (x - v) & 0xff)
+      end
+      data
+    end
+  end
+  p.output 'glitch_png/transpose_average_filter.png'
+end
