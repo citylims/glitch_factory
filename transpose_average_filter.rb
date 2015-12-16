7f9e7236c3c5eb458db9af661003db1b36e674da
Ausin Formica <austin.formica@gmail.com>
2015-12-16 11:40:29 -0500

    rm puts options

--- transpose_average_filter.rb
+++ transpose_average_filter.rb
@@ -1,10 +1,7 @@
 require 'pnglitch'
 
 PNGlitch.open('source_png/NWA.png') do |p|
-
   p.change_all_filters 3
-  options = [p.filter.to_s]
-  puts options
   p.each_scanline do |l|
     l.register_filter_encoder do |data, prev|
       data.size.times.reverse_each do |i|
